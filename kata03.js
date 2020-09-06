ee4f582f5bc748b8bbc678ade603f0cc822808cc
Jason Melnikel <jmelnikel@gmail.com>
2020-09-06 15:54:08 -0400

    kata03.js

---
+++ kata03.js
@@ -0,0 +1,20 @@
+// Vowels
+// Count the number of vowels (a, e, i, o, and u) that appear in a given string.
+
+const numberOfVowels = function(data) {
+  let number = 0;
+  let vowels = ["a", "e", "i", "o", "u"];
+
+  for (let char of data) {
+    for (let letter of vowels) {
+      if (char === letter) {
+        number += 1;
+      }
+    }
+  }
+  return number
+};
+
+console.log(numberOfVowels("orange"));
+console.log(numberOfVowels("lighthouse labs"));
+console.log(numberOfVowels("aeiou"));
