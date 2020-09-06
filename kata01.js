ae927f3ee1b72e6d8282dbd86d90ffe3821ebeaf
Jason Melnikel <jmelnikel@gmail.com>
2020-09-06 14:42:21 -0400

    Kata01

---
+++ kata01.js
@@ -0,0 +1,12 @@
+// Sum the Largest Numbers
+// Find the two largest numbers in the array, and sum them together.
+
+
+const sumLargestNumbers = function(data) {
+  data.sort((a, b) => b - a)
+  return data[0] + data [1]
+};
+
+console.log(sumLargestNumbers([1, 10]));
+console.log(sumLargestNumbers([1, 2, 3]));
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
