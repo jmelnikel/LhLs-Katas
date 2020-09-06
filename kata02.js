bdefb141c538a0a43e46331461c9a6b11c76ed13
Jason Melnikel <jmelnikel@gmail.com>
2020-09-06 15:28:40 -0400

    kata02

---
+++ kata02.js
@@ -0,0 +1,24 @@
+// Conditional Sums
+// Add only the numbers in the array which match the given condition (even or odd).
+
+const conditionalSum = function(values, condition) {
+  if (condition === "even") {
+    let filtered = values.filter(num => num % 2 === 0);
+    if (filtered.length === 0) return 0;
+    sum = filtered.reduce((num, sum) => num + sum);
+  } else {
+    let filtered = values.filter(num => num % 2 !== 0);
+    if (filtered.length === 0) return 0;
+    sum = filtered.reduce((num, sum) => num + sum);
+  }
+  return sum;
+};
+
+
+
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
+console.log(conditionalSum([2], "odd"));
+console.log(conditionalSum([1], "even"));
