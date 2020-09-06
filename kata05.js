a9b5ac9a8772715ff5b57fca33b408d19261d316
Jason Melnikel <jmelnikel@gmail.com>
2020-09-06 16:31:10 -0400

    kata05

---
+++ kata05.js
@@ -0,0 +1,10 @@
+// Percent Encoded String
+// Given a normal string of words, turn it into a percent-encoded string by replacing all whitespace with %20.
+
+const urlEncode = function(text) {
+  return text.trim().split(" ").join("%20")
+};
+
+console.log(urlEncode("Lighthouse Labs"));
+console.log(urlEncode(" Lighthouse Labs "));
+console.log(urlEncode("blue is greener than purple for sure"));
