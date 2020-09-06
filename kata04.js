80027567ad44854d06a8041234a641afdc65791c
Jason Melnikel <jmelnikel@gmail.com>
2020-09-06 16:17:16 -0400

    kata04

---
+++ kata04.js
@@ -0,0 +1,26 @@
+// Instructors' names
+// You are given a list of instructors and have to determine which instructor has the longest name.
+
+const instructorWithLongestName = function(instructors) {
+  let nameLength = 0;
+  let returnValue
+  for (let item of instructors) {
+    if (item["name"].length > nameLength) {
+      nameLength = item["name"].length;
+      returnValue = item;
+    }
+  }
+  return returnValue
+};
+
+console.log(instructorWithLongestName([
+  {name: "Samuel", course: "iOS"},
+  {name: "Jeremiah", course: "Web"},
+  {name: "Ophilia", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+console.log(instructorWithLongestName([
+  {name: "Matthew", course: "Web"},
+  {name: "David", course: "iOS"},
+  {name: "Domascus", course: "Web"}
+]));
