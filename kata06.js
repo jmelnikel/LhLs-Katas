5c564fce63ccc1a190837361dd07a72e7721256a
Jason Melnikel <jmelnikel@gmail.com>
2020-09-08 11:46:08 -0400

    attempt kata06

---
+++ kata06.js
@@ -0,0 +1,36 @@
+// Smart Parking
+// Write a function called whereCanIPark() that returns the coordinates of an available parking spot for the vehicle, or returns false if there is no available spot. Our function receives an array of arrays representing parking spots, and a string with type of the vehicle that is looking for a parking spot. An upper-case letter means that the particular spot is AVAILABLE, while lower-case letters mean that the spot is UNAVAILABLE.
+
+const whereCanIPark = function (spots, vehicle) {
+  const regular = "R";
+  const small = "RS";
+  const motorcycle = "RSM";
+  if (vehicle === "regular") {
+    vehicle = regular;
+  } else if (vehicle === "small") {
+    vehicle = small;
+  } else if (vehicle === "motorcycle") {
+    vehicle = motorcycle;
+  }
+  for (let lineIndex of spots) {
+    for (let spotIndex of line) {
+      if (line[spotIndex].incudes(vehicle)) {
+
+      }
+    }
+  }
+};
+
+console.log(whereCanIPark(
+  [
+    // COLUMNS ARE X
+    // 0    1    2    3    4    5
+    ['s', 's', 's', 'S', 'R', 'M'], // 0 ROWS ARE Y
+    ['s', 'M', 's', 'S', 'r', 'M'], // 1
+    ['s', 'M', 's', 'S', 'r', 'm'], // 2
+    ['S', 'r', 's', 'm', 'r', 'M'], // 3
+    ['S', 'r', 's', 'm', 'r', 'M'], // 4
+    ['S', 'r', 'S', 'M', 'M', 'S']  // 5
+  ],
+  'regular'
+));
