e82407a0b49a6899c770ce9c539bf2dbe91d84ff
Jason Melnikel <jmelnikel@gmail.com>
2020-09-08 13:55:55 -0400

    kata06

--- kata06.js
+++ kata06.js
@@ -2,24 +2,23 @@
 // Write a function called whereCanIPark() that returns the coordinates of an available parking spot for the vehicle, or returns false if there is no available spot. Our function receives an array of arrays representing parking spots, and a string with type of the vehicle that is looking for a parking spot. An upper-case letter means that the particular spot is AVAILABLE, while lower-case letters mean that the spot is UNAVAILABLE.
 
 const whereCanIPark = function (spots, vehicle) {
-  const regular = "R";
-  const small = "RS";
-  const motorcycle = "RSM";
-  if (vehicle === "regular") {
-    vehicle = regular;
+  if (vehicle === "motorcycle") {
+    findSpot(spots, ["R", "S", "M"])
   } else if (vehicle === "small") {
-    vehicle = small;
-  } else if (vehicle === "motorcycle") {
-    vehicle = motorcycle;
-  }
-  for (let lineIndex of spots) {
-    for (let spotIndex of line) {
-      if (line[spotIndex].incudes(vehicle)) {
-
-      }
-    }
+    findSpot(spots, ["R", "S"])
+  } else {
+    findSpot(spots, ["R"])
   }
 };
+
+const findSpot = function (array, criteria) {
+  // console.log(array);
+  console.log(criteria);
+  for (let lineIndex in array) {
+    console.log(array[lineIndex])
+  }
+}
+
 
 console.log(whereCanIPark(
   [
