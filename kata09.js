1cf361f60734428ebb4108bcf678ae28b75d8877
Jason Melnikel <jmelnikel@gmail.com>
2020-09-09 13:57:26 -0400

    kata09

--- kata09.js
+++ kata09.js
@@ -1,15 +1,26 @@
 // Case Maker
 // Create a function that when it receives a normal string of words separated with spaces as the input. Our job is to convert these strings into camel cased strings.
 
+
 const camelCase = function(input) {
-  let output = input.split(" ")
-  for (let i = 1; i <= output.length - 1; i++) {
-    
-    
+  let arrayOfStrings = input.split(" ");
+  let arrayOfArrays = []
+  for (let string of arrayOfStrings) {
+    let stringArray = string.split("")
+    arrayOfArrays.push(stringArray)
   }
-  console.log(output)
-   
+  for (let i = 1; i <= arrayOfArrays.length - 1; i++) {
+    arrayOfArrays[i][0] = arrayOfArrays[i][0].toUpperCase()
+  }
+  let outputArray = []
+  for (let stringArray of arrayOfArrays) {
+    let string = stringArray.join("")
+    outputArray.push(string)
+  }
+  return outputArray.join("")
 };
+
+
 
 console.log(camelCase("this is a string"));
 console.log(camelCase("loopy lighthouse"));
