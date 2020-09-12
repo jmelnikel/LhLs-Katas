c019c463bef341902468f57e3f7e2b0afaa867b5
Jason Melnikel <jmelnikel@gmail.com>
2020-09-12 13:22:16 -0400

    starting kata14

---
+++ kata14.js
@@ -0,0 +1,25 @@
+// Change Calculator
+// Create a function calculateChange that when given two numbers, 1)the total of a transaction, and 2)the amount of cash given to the cashier; should return an object which describes the total amount of change for the cashier to give back.
+
+/* Both of these numbers will be represented as whole numbers in cents. Therefore $10 will be represented as 1000.
+
+Valid denominations are as follows:
+
+Twenty dollars
+Ten dollars
+Five dollars
+Two dollars
+One dollar
+Quarter (25¢)
+Dime (10¢)
+Nickel (5¢)
+Penny (1¢)
+*/
+
+const calculateChange = function(total, cash) {
+  // Your code here
+};
+
+console.log(calculateChange(1787, 2000));
+console.log(calculateChange(2623, 4000));
+console.log(calculateChange(501, 1000));
