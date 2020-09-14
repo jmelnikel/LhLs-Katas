f6fb8e64f8b838834347ec9b36bb185efdb3fe9d
Jason Melnikel <jmelnikel@gmail.com>
2020-09-14 13:24:09 -0400

    kata15

---
+++ kata15.js
@@ -0,0 +1,27 @@
+// Organizing Instructors
+// Given a list of instructors, create a single object to organize them based on their course.
+
+const organizeInstructors = function(instructors) {
+  let output = {};
+  for (let instructor of instructors) {
+    if (!output[instructor["course"]]) {
+      output[instructor["course"]] = [instructor["name"]]
+    } else {
+      output[instructor["course"]].push(instructor["name"])
+    }
+  }
+  return output;
+};
+
+console.log(organizeInstructors([
+  {name: "Samuel", course: "iOS"},
+  {name: "Victoria", course: "Web"},
+  {name: "Karim", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+console.log(organizeInstructors([
+  {name: "Brendan", course: "Blockchain"},
+  {name: "David", course: "Web"},
+  {name: "Martha", course: "iOS"},
+  {name: "Carlos", course: "Web"}
+]));
