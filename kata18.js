fb2f575b35a63ac2e8eb60ff3dead630b8ed81c2
Jason Melnikel <jmelnikel@gmail.com>
2020-09-17 12:39:02 -0400

    kata18

--- kata18.js
+++ kata18.js
@@ -2,39 +2,52 @@
 // Creating a function that when given a string it returns Square Code. In Square Code, the spaces are removed from the english text and the characters are written into a rectangle. The message is then re-coded by reading down the columns going left to right.
 
 const squareCode = function(message) {
-  message = message.split(" ").join("").split("")
-  let magicNumber = Math.ceil(Math.sqrt(message.length))
+  let array = message.split(" ").join("").split("");
+  let num = Math.ceil(Math.sqrt(array.length));
+  let tempArray
+  let finalArray = [];
   
-  console.log(createLetterBundle(message, magicNumber));
-
+  let createArray = function (array) {
+    if (array.length === 0) {
+      return;
+    } else if (array.length < num) {
+      finalArray.push(array);
+      return;
+    } else {
+      tempArray = [];
+      for (i = 0; i < num; i++) {
+        tempArray.push(array.shift());
+      };
+      finalArray.push(tempArray);
+    };
+    createArray(array);
+  };
+  // console.log("This is array", array)
+  createArray(array);
+  
+  let line = "";
+  let createLine = function (finalArray) {
+    if (finalArray[0].length === 0) {
+      return;
+    } else {
+      finalArray.forEach(function (subArray) {
+        if (subArray.length === 0) {
+          return;
+        } else {
+          line += subArray.shift();
+          return line;
+        };
+      });
+      line += " ";
+    };
+    
+    createLine(finalArray);
+    return line;
+  };
+  return createLine(finalArray);
 };
 
-
-
-// clu hlt io
-// fto ehg ee dd
-
 console.log(squareCode("chill out"));
-// console.log(squareCode("feed the dog"));
-// console.log(squareCode("have a nice day"));
-// console.log(squareCode("if man was meant to stay on the ground god would have given us roots"));
-
-
-function createLetterBundle(array, number) {
-  let letterBundle = [];
-  if (array.length < number) {
-    for (let letter of array) {
-      letterBundle.push(array.shift())
-    }
-  } else {
-    for (let i = 0; i < number; i++) {
-      letterBundle.push(array.shift())
-    }
-  }
-  console.log(array)
-  return letterBundle // [ "a", "b", "c" ]
-}
-
-function cutUpArray(array, number) {
-  
-}
+console.log(squareCode("feed the dog"));
+console.log(squareCode("have a nice day"));
+console.log(squareCode("if man was meant to stay on the ground god would have given us roots"));
