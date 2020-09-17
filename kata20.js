ed9095cb0ed673c7371618cd7951c47871648083
Jason Melnikel <jmelnikel@gmail.com>
2020-09-17 17:44:34 -0400

    kata20

--- kata20.js
+++ kata20.js
@@ -2,88 +2,69 @@
 // Write a function to help taxicabs determine how far away a destination is based on the directions given.
 
 const blocksAway = function(directions) {
-  const rollNumber = (directions.length / 2)
+  const rollNumber = (directions.length / 2);
   const arrayOfArrays = [];
   for (let i = 1; i <= rollNumber; i++) {
     const tempArray = [];
-    tempArray.push(directions.shift())
-    tempArray.push(directions.shift())
-    arrayOfArrays.push(tempArray)
-  }
-  // console.log("This is arrayOfArrays --->", arrayOfArrays)
-
-  const directionSet1 = [];
-  const directionSet2 = [];
-  for (let array in arrayOfArrays)
-  if (array % 2 === 0) {
-    directionSet1.push(arrayOfArrays[array]);
+    tempArray.push(directions.shift());
+    tempArray.push(directions.shift());
+    arrayOfArrays.push(tempArray);
+  };
+  
+  const distance = { "east": 0, "north": 0 };
+  let currentDirection = "";
+  if (arrayOfArrays[0][0] === "right") {
+    currentDirection = "east";
+    distance["east"] += arrayOfArrays[0][1];
   } else {
-    directionSet2.push(arrayOfArrays[array]);
-  }
-  console.log("This is directionSet1 --->", directionSet1)
-  console.log("This is directionSet2 --->", directionSet2) 
+    currentDirection = "north";
+    distance["north"] += arrayOfArrays[0][1];
+  };
 
-  let startDirection = ""
-  if (directionSet1[0][0] === "right") {
-    startDirection = "east";
+  for (let index = 1; index < arrayOfArrays.length; index++) {
+    currentDirection = determineDirection(currentDirection, arrayOfArrays[index][0]);
+    if (currentDirection === "north") {
+      distance["north"] += arrayOfArrays[index][1];
+    };
+    if (currentDirection === "east") {
+      distance["east"] += arrayOfArrays[index][1];
+    };
+    if (currentDirection === "south") {
+      distance["north"] -= arrayOfArrays[index][1];
+    };
+    if (currentDirection === "west") {
+      distance["east"] -= arrayOfArrays[index][1];
+    };
   }
-  if (directionSet1[0][0] === "left"){
-    startDirection = "north"
-  }
-  console.log(startDirection)
-  
-  const distance = { "east": 0, "north": 0 }
-  if (startDirection === "east") {
-    let eastBlockCounter = 0;
-    for (let item of directionSet1) {
-      if (item[0] === "right") {
-        eastBlockCounter += item[1];
-      }
-      if (item[0] === "left") {
-        eastBlockCounter -= item[1];
-      }
-      distance["east"] = eastBlockCounter
-    }
-    let northBlockCounter = 0;
-    for (let item of directionSet2) {
-      if (item[0] === "left") {
-        northBlockCounter += item[1];
-      }
-      if (item[0] === "right") {
-        northBlockCounter -= item[1];
-      }
-      distance["north"] = northBlockCounter
-    }
-  }
-  if (startDirection === "north") {
-    let northBlockCounter = 0;
-    for (let item of directionSet1) {
-      if (item[0] === "left") {
-        northBlockCounter += item[1];
-      }
-      if (item[0] === "right") {
-        northBlockCounter -= item[1];
-      }
-      distance["north"] = northBlockCounter
-    }
-    let eastBlockCounter = 0;
-    for (let item of directionSet2) {
-      if (item[0] === "right") {
-        eastBlockCounter += item[1];
-      }
-      if (item[0] === "left") {
-        eastBlockCounter -= item[1];
-      }
-      distance["east"] = eastBlockCounter
-    }
-  }
-
-  return distance
-  
+  return distance;
 };
 
-
-// console.log(blocksAway(["right", 2, "left", 3, "left", 1]));
-// console.log(blocksAway(["left", 1, "right", 1, "left", 1, "right", 1, "left", 1, "right", 1]));
+console.log(blocksAway(["right", 2, "left", 3, "left", 1]));
+console.log(blocksAway(["left", 1, "right", 1, "left", 1, "right", 1, "left", 1, "right", 1]));
 console.log(blocksAway(["left", 3, "right", 1, "right", 3, "right", 1]));
 
+
+function determineDirection(initialDirection, turn) {
+  let finalDirection = "";
+  if (initialDirection === "north" && turn === "right") {
+    finalDirection = "east";
+  } else if (initialDirection === "north" && turn === "left") {
+    finalDirection = "west";
+  };
+  if (initialDirection === "east" && turn === "right") {
+    finalDirection = "south";
+  } else if (initialDirection === "east" && turn === "left") {
+    finalDirection = "north";
+  };
+  if (initialDirection === "south" && turn === "right") {
+    finalDirection = "west";
+  } else if (initialDirection === "south" && turn === "left") {
+    finalDirection = "east";
+  };
+  if (initialDirection === "west" && turn === "right") {
+    finalDirection = "north";
+  } else if (initialDirection === "west" && turn === "left") {
+    finalDirection = "south";
+  };
+  return finalDirection;
+};
