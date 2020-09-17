833b1c8b0ab2480ff681183850751df1f5409ceb
Jason Melnikel <jmelnikel@gmail.com>
2020-09-17 16:49:22 -0400

    so close with kata20 but so far

--- kata20.js
+++ kata20.js
@@ -4,35 +4,86 @@
 const blocksAway = function(directions) {
   const rollNumber = (directions.length / 2)
   const arrayOfArrays = [];
-  
   for (let i = 1; i <= rollNumber; i++) {
     const tempArray = [];
     tempArray.push(directions.shift())
     tempArray.push(directions.shift())
     arrayOfArrays.push(tempArray)
   }
-  console.log(arrayOfArrays) // [[ 'right', 2 ], [ 'left', 3 ], [ 'left', 1 ]]
+  // console.log("This is arrayOfArrays --->", arrayOfArrays)
 
-  const directionSetEven = [];
-  const directionSetOdd = [];
+  const directionSet1 = [];
+  const directionSet2 = [];
   for (let array in arrayOfArrays)
   if (array % 2 === 0) {
-    directionSetEven.push(arrayOfArrays[array]);
+    directionSet1.push(arrayOfArrays[array]);
   } else {
-    directionSetOdd.push(arrayOfArrays[array]);
+    directionSet2.push(arrayOfArrays[array]);
   }
-  console.log(directionSetEven) // [[ 'right', 2 ], [ 'left', 1 ]]
-  console.log(directionSetOdd) // [[ 'left', 3 ]]
+  console.log("This is directionSet1 --->", directionSet1)
+  console.log("This is directionSet2 --->", directionSet2) 
 
+  let startDirection = ""
+  if (directionSet1[0][0] === "right") {
+    startDirection = "east";
+  }
+  if (directionSet1[0][0] === "left"){
+    startDirection = "north"
+  }
+  console.log(startDirection)
   
+  const distance = { "east": 0, "north": 0 }
+  if (startDirection === "east") {
+    let eastBlockCounter = 0;
+    for (let item of directionSet1) {
+      if (item[0] === "right") {
+        eastBlockCounter += item[1];
+      }
+      if (item[0] === "left") {
+        eastBlockCounter -= item[1];
+      }
+      distance["east"] = eastBlockCounter
+    }
+    let northBlockCounter = 0;
+    for (let item of directionSet2) {
+      if (item[0] === "left") {
+        northBlockCounter += item[1];
+      }
+      if (item[0] === "right") {
+        northBlockCounter -= item[1];
+      }
+      distance["north"] = northBlockCounter
+    }
+  }
+  if (startDirection === "north") {
+    let northBlockCounter = 0;
+    for (let item of directionSet1) {
+      if (item[0] === "left") {
+        northBlockCounter += item[1];
+      }
+      if (item[0] === "right") {
+        northBlockCounter -= item[1];
+      }
+      distance["north"] = northBlockCounter
+    }
+    let eastBlockCounter = 0;
+    for (let item of directionSet2) {
+      if (item[0] === "right") {
+        eastBlockCounter += item[1];
+      }
+      if (item[0] === "left") {
+        eastBlockCounter -= item[1];
+      }
+      distance["east"] = eastBlockCounter
+    }
+  }
 
-
-
-
+  return distance
+  
 };
 
 
-console.log(blocksAway(["right", 2, "left", 3, "left", 1]));
+// console.log(blocksAway(["right", 2, "left", 3, "left", 1]));
 // console.log(blocksAway(["left", 1, "right", 1, "left", 1, "right", 1, "left", 1, "right", 1]));
-// console.log(blocksAway(["left", 3, "right", 1, "right", 3, "right", 1]));
+console.log(blocksAway(["left", 3, "right", 1, "right", 3, "right", 1]));
 
