8e5670064e8917a7573b2b636834446f7167005d
Jason Melnikel <jmelnikel@gmail.com>
2020-09-17 12:46:44 -0400

    starting kata19

---
+++ kata19.js
@@ -0,0 +1,10 @@
+// Quuen Threat Detector
+// Write a function that will detect if two queens on a chess board can attack each other.
+
+const generateBoard = function() {
+
+};
+
+const queenThreat = function() {
+
+};
