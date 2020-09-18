ed0ecb4cd55d379da74c114491cdc153d521c7b1
Jason Melnikel <jmelnikel@gmail.com>
2020-09-18 10:44:32 -0400

    Attempt kata21 - needs CL input

---
+++ kata21.js
@@ -0,0 +1,4 @@
+// Number Guesser
+// Write a function where the user has to guess a secret number. After every guess the program tells the user whether their number was too large or too small. At the end, the number of tries needed should be printed.
+
+// Inputting the same number multiple times should only count as one try. If the user provides an answer which isn't a number, print an error message and do not count this as a try.
