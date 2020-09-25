5a836409f96f1e6b814c9ca7d2ed73a5e28e875d
Jason Melnikel <jmelnikel@gmail.com>
2020-09-25 14:50:31 -0400

    kata22

---
+++ kata22.js
@@ -0,0 +1,17 @@
+// Reversing a String
+// Write a function that takes any number of command line arguments, all strings, and reverses them before outputting them one at a time to the console.
+
+let args = process.argv;
+args.splice(0, 2);
+
+const reverseString = function(string) {
+  let output = ""
+  for (let i = string.length - 1; i >= 0; i--) {
+    output += string[i]
+  }
+  return output
+}
+
+for (let item of args) {
+  console.log(reverseString(item))
+}
