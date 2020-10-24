f144a067c04c0287613b9ed9e99fe22770a24129
Jason Melnikel <jmelnikel@gmail.com>
2020-10-24 15:47:21 -0400

    kata32

---
+++ kata32.js
@@ -0,0 +1,19 @@
+// Poppin Bottles
+// Given the parameters below, write a program so that you can figure out how many total bottles of pop can be redeemed given a customer investment.
+// 1) For every two empty bottles, you can get one free (full) bottle of pop.
+// 2) For every four bottle caps, you can get one free (full) bottle of pop.
+// 3) Each bottle of pop costs $2 to purchase.
+
+
+
+// let args = process.argv.slice(2);
+
+let bottles = 1
+const totalBottles = bottles => {
+  let tally = {
+    bottles
+  }
+
+};
+
+console.log(totalBottles(args));
